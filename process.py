86c51c74b50ca408a2d0369a1025df8f84bd3844
olojo-99 <74866572+olojo-99@users.noreply.github.com>
2022-09-18 12:17:22 +0100

    Update process.py

--- process.py
+++ process.py
@@ -33,10 +33,3 @@ def process_obj(tasks):
         exit("-> No valid processes to run") # Exit the program with error message
 
     return p_objs # return list of processes object 
-
-
-'''
-I certify that this assignment is my own work carried out in a manner pursuant to the DCU Academic Integrity Policy,
-based on my personal study and/or research and that I have acknowledged all material and sources used in its preparation.
-Benjamin Olojo - 19500599
-'''
