d93d136ac06a594d1d6fb9a8f35ce2f83ac17bfc
olojo-99 <74866572+olojo-99@users.noreply.github.com>
2022-09-18 12:17:38 +0100

    Update rr.py

--- rr.py
+++ rr.py
@@ -65,10 +65,3 @@ process_details(processes)
 
 # Print details regarding average wait and turnaround times
 avg_details(processes)
-
-
-'''
-I certify that this assignment is my own work carried out in a manner pursuant to the DCU Academic Integrity Policy,
-based on my personal study and/or research and that I have acknowledged all material and sources used in its preparation.
-Benjamin Olojo - 19500599
-'''
